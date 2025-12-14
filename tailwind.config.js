636ce1576134a002d67ce94dcb46febfe4662081
Adinug122 <adi315429@gmail.com>
2025-12-14 16:34:03 +0700

    Fix component

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,25 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
-import colors from 'tailwindcss/colors'; 
+import defaultTheme from 'tailwindcss/defaultTheme'
+import colors from 'tailwindcss/colors'
 
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
-    ],
+  content: [
+    './resources/views/**/*.blade.php',
+    './resources/views/**/*.php',
+    './resources/views/components/**/*.blade.php',
+    './app/View/Components/**/*.php',
+  ],
 
-    theme: {
-        extend: {
-            fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
-            },
-            colors: {
-                
-                brand: colors.blue, 
-            },
-        },
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        brand: colors.blue,
+      },
     },
+  },
 
-    plugins: [require('@tailwindcss/forms')],
-};
+  plugins: [require('@tailwindcss/forms')],
+}
